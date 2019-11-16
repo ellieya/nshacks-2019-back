6458b2bbf4d0f0b77011925f13f46398458458c7
Jorge Quiroz <gerschjr@gmail.com>
2019-11-16 06:54:16 -0500

    u

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-// const spawn = require("child_process").spawn;
-
-// const process = spawn("python", ["s2t.py"])
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8000;
-
-// // python script
-// app.get('/', (req, res) => {
-
-//   const { spawn } = require('child_process');
-//   const pyProg = spawn('python', ['s2t.py']);
-
-//   pyProg.stdout.on('data', function(data) {
-
-//       console.log(data.toString());
-//       res.write(data);
-//       res.end('end');
-//   });
-// })
-
 
 // Middleware
 app.use(cors());
--- s2t.py
+++ s2t.py
@@ -1,27 +1,27 @@
-import speech_recognition as sr
-import sys
+# import speech_recognition as sr
+# import sys
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print('Begin speaking')
-    audio = r.listen(source)
+# r = sr.Recognizer()
+# with sr.Microphone() as source:
+#     print('Begin speaking')
+#     audio = r.listen(source)
 
-try:
-    print('You said: ' + r.recognize_google(audio))
-    wordsArr = r.recognize_google(audio).split(" ")
-    print(wordsArr)
-except:
-    pass
+# try:
+#     print('You said: ' + r.recognize_google(audio))
+#     wordsArr = r.recognize_google(audio).split(" ")
+#     print(wordsArr)
+# except:
+#     pass
 
-def search():
-    keywords = ['food', 'shelter', 'drugs', 'church', 'hospital']
-    for word in wordsArr:
-        if word in keywords:
-            return word
-    return -1
+# def search():
+#     keywords = ['food', 'shelter', 'drugs', 'church', 'hospital']
+#     for word in wordsArr:
+#         if word in keywords:
+#             return word
+#     return -1
 
-result = search()
-print(result)
+# result = search()
+# print(result)
 
 
         
